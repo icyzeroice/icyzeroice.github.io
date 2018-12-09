33f4bd915ce21d816e6afd2209d489f45065583c
icyzeroice <ziv3@outlook.com>
2018-12-09 14:42:18 +0800

    :sweat_drops: use Single Instance Pattern

--- src/EventTest/EventTest.js
+++ src/EventTest/EventTest.js
@@ -1,12 +1,70 @@
-window.addEventListener('keydown', eventDecorator('keydown'))
-window.addEventListener('click', eventDecorator('click'))
-window.addEventListener('mousedown', eventDecorator('mousedown'))
+const EventTest = (function () {
+  
+  const __TYPE__ = {
+    
+    // MouseEvent
+    CLICK: 'click',
+    MOUSEDOWN: 'mousedown',
 
-function eventDecorator(label, fn) {
-
-  return function eventPrinter(e) {
-    label ? console.log(`%c[${label}]`, 'color:#0000ff', e)
-          : console.log(e)
-    fn && fn(e)
+    // KeyboardEvent
+    KEYDOWN: 'keydown',
   }
+
+  const _types = []
+  const _fns = []
+
+  return {
+    
+    __TYPE__,
+
+    add(label, cb) {
+      if (!label) return null;
+
+      const fn = e => {
+        console.log(`%c[${label}]`, 'color:#ff00ff', e)
+        cb && cb(e)
+      }
+
+      window.addEventListener(label, fn)
+
+      _types.push(label)
+      _fns.push(fn)
+
+      return _fns.length
+    },
+
+    remove(handle) {
+      // while handle is `null`
+      if (!handle) return null;
+
+      const label = _types[handle - 1]
+      const fn = _fns[handle - 1]
+
+      window.removeEventListener(label, fn)
+    },
+
+    clear(label) {      
+      let counter = _fns.length
+      
+      while (counter > 0) {
+        if (label && label !== _types[counter - 1]) {
+          counter--
+          continue
+        }
+        this.remove(counter--)
+      }
+
+      console.log(`clear ${label} listeners`)
+    }
+  }
+})()
+
+EventTest.add(EventTest.__TYPE__.CLICK)
+EventTest.add(EventTest.__TYPE__.MOUSEDOWN)
+EventTest.add(EventTest.__TYPE__.KEYDOWN)
+
+/*
+function tapEffect(x, y) {
+  const a = document.createElement()
 }
+*/
