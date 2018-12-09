01c6f6c3a51b533fd7fa33a71891e6fc7a4fae5d
icyzeroice <ziv3@outlook.com>
2018-12-09 18:33:34 +0800

    :arrow_up: update Toast

--- src/EventTest/EventTest.js
+++ src/EventTest/EventTest.js
@@ -59,12 +59,79 @@ const EventTest = (function () {
   }
 })()
 
-EventTest.add(EventTest.__TYPE__.CLICK)
-EventTest.add(EventTest.__TYPE__.MOUSEDOWN)
-EventTest.add(EventTest.__TYPE__.KEYDOWN)
+const Toast = (function () {
+  
+  let _inst = null
+
+  class _Toast {
+    constructor() {
+      this.target = null
+      this._handle = null
+
+      this._init()
+    }
+
+    _init() {
+      this.target = document.createElement('p')
+      this.target.className = 'toast'
+      this._off()
+      document.body.appendChild(this.target)
+    }
+
+    _on() {
+      this.target.style.display = 'block'
+    }
+
+    _off() {
+      this.target.style.display = 'none'
+      this._handle = null
+    }
+
+    _text(mes) {
+      this.target.innerText = mes
+    }
+
+    _setTime() {
+      this._handle = setTimeout(() => {
+        this._off()
+      }, 1500)
+    }
+
+    _refreshTime() {
+      clearTimeout(this._handle)
+      this._setTime()
+    }
+
+    display(mes) {
+
+      if (!this._handle) {
+        this._text(mes)
+        this._on()
+        this._setTime()
+      }
+
+      else {
+        this._text(mes)
+        this._refreshTime()
+      }
+    }
+  }
+
+  return function () {
+    return _inst || new _Toast()
+  }
+})()
 
 /*
 function tapEffect(x, y) {
   const a = document.createElement()
 }
 */
+
+const toast = new Toast()
+
+
+EventTest.add(EventTest.__TYPE__.CLICK, e => toast.display(`(${e.x}, ${e.y}), client(${e.clientX}, ${e.clientY}), layer(${e.layerX}, ${e.layerY}), offset(${e.offsetX}, ${e.offsetY}), page(${e.pageX}, ${e.pageY}), screen(${e.screenX}, ${e.screenY})`))
+EventTest.add(EventTest.__TYPE__.MOUSEDOWN, e => toast.display(`(${e.x}, ${e.y}), client(${e.clientX}, ${e.clientY}), layer(${e.layerX}, ${e.layerY}), offset(${e.offsetX}, ${e.offsetY}), page(${e.pageX}, ${e.pageY}), screen(${e.screenX}, ${e.screenY})`))
+EventTest.add(EventTest.__TYPE__.KEYDOWN, e => toast.display(`key: ${e.key}, code: ${e.code}`))
+
